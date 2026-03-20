6b9f2314c0732f18723c4135f958416794a9fd34
Goooble <bivinbheemaiah@gmail.com>
2026-03-21 00:22:49 +0530

    about: adjust text size

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -4,11 +4,11 @@ function Hero() {
       className="h-dvh relative bg-[url('src/assets/hero.jpeg')] bg-cover bg-[position:20%_0%] xl:bg-left shadow-[inset_0_0_0_1000px_rgba(0,0,0,0.5)] flex items-center justify-end"
       id="home"
     >
-      <div className="flex flex-col justify-center mx-auto lg:mr-[10%] mb-50 2xl:my-50">
+      <div className="flex flex-col justify-center mx-auto lg:mr-[10%] mb-50 my-60 lg:my-50">
         <p className="mb-[1%] text-1xl xl:text-2xl 2xl:text-3xl text-white">
           COMMANDING THE SPOTLIGHT
         </p>
-        <p className="text-5xl xl:text-6xl  2xl:text-9xl font-extrabold text-white">
+        <p className="text-5xl md:text-7xl 2xl:text-9xl font-extrabold text-white">
           JASVANTH V A
         </p>
         <p className="text-4xl xl:text-5xl 2xl:text-8xl text-white">Anchor</p>
